5c99b14530d1b7e636c3f962aef88a3d14c3da84
Liz Peacock <liz.peacock11@gmail.com>
2017-02-06 15:09:50 -0600

    created profile jsp and profile servlet login redirects to profile with proper credentials

---
+++ src/main/java/ProfileServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by user on 2/6/17.
+ */
+@WebServlet(name = "ProfileServlet", urlPatterns = "/profile")
+public class ProfileServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.getRequestDispatcher("/profile.jsp").forward(req, res);
+    }
+}
---
+++ src/main/webapp/profile.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 2/6/17
+  Time: 2:34 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <jsp:include page="partials/head.jsp">
+        <jsp:param name="title" value="Profile" />
+    </jsp:include>
+</head>
+<body>
+    <jsp:include page="partials/navbar.jsp" />
+    <div class="container">
+        <h1>User Profile</h1>
+            <div>
+                <p>Welcome user</p>
+            </div>
+    </div>
+</body>
+</html>
